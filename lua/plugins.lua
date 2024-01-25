f9becb57e12d225a5ec74ee5f370551383e9494a
tongteo <vinhtrung1412@gmail.com>
2024-01-25 08:52:24 +0000

    add surround plugins

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,6 +1,15 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
+  use({
+    "kylechui/nvim-surround",
+    tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+    config = function()
+        require("nvim-surround").setup({
+            -- Configuration here, or leave empty to use defaults
+        })
+    end
+})
 -- using packer.nvim
   use 'mfussenegger/nvim-dap'
   use { "rcarriga/nvim-dap-ui", requires = {"mfussenegger/nvim-dap"} }
